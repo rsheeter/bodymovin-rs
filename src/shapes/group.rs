5d61c7e0b37b59e210e4982e3c7e76b7cc006fb3
Rod S <rsheeter@google.com>
2023-12-19 21:30:19 -0800

    Default for group and transform

--- src/shapes/group.rs
+++ src/shapes/group.rs
@@ -1,7 +1,7 @@
 use crate::shapes;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct Group {
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(rename = "mn")]
--- src/shapes/transform.rs
+++ src/shapes/transform.rs
@@ -2,22 +2,38 @@ use crate::properties;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(default)]
 pub struct Transform {
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(rename = "nm")]
     pub name: Option<String>,
-    #[serde(rename = "a", default = "properties::MultiDimensional::zero")]
+    #[serde(rename = "a")]
     pub anchor_point: properties::MultiDimensional,
-    #[serde(rename = "p", default = "properties::MultiDimensional::zero")]
+    #[serde(rename = "p")]
     pub position: properties::MultiDimensional,
-    #[serde(rename = "s", default = "properties::MultiDimensional::hundred")]
+    #[serde(rename = "s")]
     pub scale: properties::MultiDimensional,
-    #[serde(rename = "r", default)]
+    #[serde(rename = "r")]
     pub rotation: properties::Scalar,
-    #[serde(rename = "o", default = "properties::Scalar::hundred")]
+    #[serde(rename = "o")]
     pub opacity: properties::Scalar,
-    #[serde(rename = "sk", default)]
+    #[serde(rename = "sk")]
     pub skew: properties::Scalar,
     #[serde(rename = "sa", default)]
     pub skew_axis: properties::Scalar,
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self {
+            name: Default::default(),
+            anchor_point: properties::MultiDimensional::zero(),
+            position: properties::MultiDimensional::zero(),
+            scale: properties::MultiDimensional::hundred(),
+            rotation: Default::default(),
+            opacity: properties::Scalar::hundred(),
+            skew: Default::default(),
+            skew_axis: Default::default(),
+        }
+    }
 }
